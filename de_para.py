416320139910f9eb59aa6827c37550a6a21099d9
Lucas Flores <lucasasflores@gmail.com>
2023-07-31 17:01:18 -0300

    bbb

--- de_para.py
+++ de_para.py
@@ -1,22 +1,25 @@
-def formatar_campos(data, mapeamento, schema):
-    if isinstance(data, list):
-        return [formatar_campos(item, mapeamento, schema) for item in data]
-    elif isinstance(data, dict):
-        novo_dicionario = {}
-        for chave, valor in data.items():
-            if chave in mapeamento:
-                novo_nome = mapeamento[chave]
-                if isinstance(valor, list) or isinstance(valor, dict):
-                    novo_valor = formatar_campos(valor, mapeamento, schema)
-                    novo_dicionario[novo_nome] = novo_valor
-                else:
-                    novo_dicionario[novo_nome] = valor
-            elif chave in schema["fields"]:
-                if isinstance(valor, list) or isinstance(valor, dict):
-                    novo_valor = formatar_campos(valor, mapeamento, schema["fields"][chave]["type"])
-                    novo_dicionario[chave] = novo_valor
-                else:
-                    novo_dicionario[chave] = valor
-        return novo_dicionario
-    else:
+def apply_mapping_and_filter(json_data, mapping, schema_avro):
+    def apply_mapping(data, mapping):
+        for old_field, new_field in mapping.items():
+            if old_field in data:
+                data[new_field] = data.pop(old_field)
         return data
+
+    def filter_data(data, schema):
+        filtered_data = {}
+        for field in schema["fields"]:
+            field_name = field["name"]
+            if field_name in data:
+                if field["type"] == "record":
+                    filtered_data[field_name] = filter_data(data[field_name], field)
+                else:
+                    filtered_data[field_name] = data[field_name]
+        return filtered_data
+
+    # Aplica o mapeamento de nomes de campos
+    json_data_mapped = apply_mapping(json_data, mapping)
+
+    # Filtra os campos mantendo apenas os campos definidos no schema Avro
+    json_data_filtered = filter_data(json_data_mapped, schema_avro)
+
+    return json_data_filtered
