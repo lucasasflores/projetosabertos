8745100fe0dceb2fd78ca6e8e30173982959a222
Lucas Flores <lucasasflores@gmail.com>
2023-07-31 16:48:23 -0300

    aa

--- de_para.py
+++ de_para.py
@@ -11,6 +11,12 @@ def formatar_campos(data, mapeamento, schema):
                     novo_dicionario[novo_nome] = novo_valor
                 else:
                     novo_dicionario[novo_nome] = valor
+            elif chave in schema["fields"]:
+                if isinstance(valor, list) or isinstance(valor, dict):
+                    novo_valor = formatar_campos(valor, mapeamento, schema["fields"][chave]["type"])
+                    novo_dicionario[chave] = novo_valor
+                else:
+                    novo_dicionario[chave] = valor
         return novo_dicionario
     else:
         return data
